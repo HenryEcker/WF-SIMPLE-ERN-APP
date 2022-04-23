2ca5e8c6c5bf4dbca93d7756978a4b01c7ca005b
HenryEcker <hde1997@gmail.com>
2022-04-22 23:15:29 -0400

    Added basic logic to process the number and apply the appropriate CSS class

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import './App.scss';
+import "./App.scss";
 import {useEffect, useState} from "react";
 
+/**
+ * @summary Determine which CSS class to apply to the table row based on `number`
+ *
+ * @param {number} number A numeric value
+ * @returns {"alert" | "warning" | "okay"} CSS class based on the value parameter
+ */
+const chooseTheme = number => {
+    if (number > 50) {
+        return "alert";
+    } else if (number > 25) {
+        return "warning";
+    } else {
+        return "okay";
+    }
+}
 
 /**
  * @summary Unpacks each object from data and produces a tr
  * @returns {JSX.Element}
  */
-const Row = (props) => {
+const Row = props => {
     const {id, name, number, description} = props;
     return (
-        <tr data-row-id={id}>
+        <tr data-row-id={id} className={chooseTheme(number)}>
             <td>{name}</td>
             <td>{number}</td>
             <td>{description}</td>
